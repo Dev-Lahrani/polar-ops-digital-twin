cb33b9860f96e4b67b4f55865f5052800a0d9777
Dev Lahrani <lahranidev10@gmail.com>
2026-09-08 09:26:18 +0530

    dev

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 import { db } from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   pages: {
